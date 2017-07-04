455ecfc4555db31b0fdfa2239fb8a92991edce29
Immersive <immrsv@users.noreply.github.com>
2017-07-04 16:31:28 +0930

    Added hinged rigid bodies, Animated ragdoll.

---
+++ Assets/SpringDecay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(HingeJoint))]
+public class SpringDecay : MonoBehaviour {
+
+
+    public float MaxStrength = 500.0f;
+    public float MinStrength = 50f;
+    public float DecayRate = 2.0f;
+
+    private HingeJoint[] Hinges;
+
+	// Use this for initialization
+	void Start () {
+        Hinges = GetComponentsInChildren<HingeJoint>();
+
+        foreach (var hinge in Hinges) {
+            var spr = hinge.spring;
+            spr.spring = MaxStrength;
+            hinge.spring = spr;
+        }
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        foreach (var hinge in Hinges) {
+            var spr = hinge.spring;
+            spr.spring = Mathf.Max(MinStrength, spr.spring - (DecayRate * Time.fixedDeltaTime));
+            hinge.spring = spr;
+        }
+    }
+}
